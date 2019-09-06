8b968d8d736c24fb45a90a2ecefea2f888d7d6ac
Jayant Paliwal <montypaliwal.biz@gmail.com>
2019-09-07 01:09:15 +0530

    RN60 and microphone permission issue and front camare issue fixed

--- src/QRScanner.js
+++ src/QRScanner.js
@@ -41,9 +41,10 @@ export default class QRScanner extends PureComponent {
     finderY: 0,         // 取景器Y轴偏移量
     zoom: 0.2,          // 缩放范围 0 - 1
     translucent: false,
-    isRepeatScan: false
+    isRepeatScan: false,
+    cameraType: 'back'
   }
-  
+
   render() {
     return (
       <View style={{
@@ -53,10 +54,12 @@ export default class QRScanner extends PureComponent {
          style={{
           flex: 1
         }}
+          captureAudio = {false}
           onBarCodeRead={this._handleBarCodeRead}
           barCodeTypes={[RNCamera.Constants.BarCodeType.qr]}
-          flashMode={!this.props.flashMode ? RNCamera.Constants.FlashMode.off : RNCamera.Constants.FlashMode.torch} 
-          zoom={this.props.zoom}>
+          flashMode={!this.props.flashMode ? RNCamera.Constants.FlashMode.off : RNCamera.Constants.FlashMode.torch}
+          zoom={this.props.zoom}
+          type={this.props.cameraType}>
           <View style={[styles.topButtonsContainer, this.props.topViewStyle]}>
             {this.props.renderTopView()}
           </View>
@@ -227,5 +230,6 @@ QRScanner.propTypes = {
   finderX: PropTypes.number,
   finderY: PropTypes.number,
   zoom: PropTypes.number,
-  translucent: PropTypes.bool
+  translucent: PropTypes.bool,
+  cameraType: PropTypes.string
 }
